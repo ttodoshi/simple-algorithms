acdac928759d6cc76ce5253ca70c9a47493e9398
Александр Шешин <sheshin.alexanderkpc2052020@gmail.com>
2022-04-08 16:23:02 +0300

    quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -1,15 +1,11 @@
-from random import randint
-
-
 def quicksort(list):
     if len(list) < 2:
         return list
     else:
         pivot = list[0]
         less = [i for i in list[1:] if i <= pivot]
-        
         greater = [i for i in list[1:] if i > pivot]
         
         return quicksort(less) + [pivot] + quicksort(greater)
     
-print(quicksort([13, 10, 1, -4, 2, 50]))
+print(quicksort([13, 10, 1, -4, 2, 50, 4 , 5, -1, 3]))
